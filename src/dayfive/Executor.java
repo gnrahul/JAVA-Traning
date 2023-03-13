04b35fcd52b8ae27e576e29671104782222f55c2
rahul <rahul@Rahul>
2023-03-13 16:21:13 +0530

    JAVA Full Stack Training - TNS

--- src/dayfive/Executor.java
+++ src/dayfive/Executor.java
@@ -3,33 +3,35 @@ package dayfive;
 public class Executor {
 
 	public static void main(String[] args) {
-		//Accessing same package class using Object
-		Base b1=new Base(); 
+		//accessing same package class
+		Base b1 =new Base();     //object
 		b1.methodDefault();
 		b1.methodProtected();
 		b1.methodPublic();
 		
 		
-		b1.varDefault=11;
-		b1.methodDefault();
-		
-		//Private Member accessing
-		
-		/*b1.varPrivate=21;
-		b1.methodPrivate();*/
-		
-		b1.varProtected=31;
-		b1.methodProtected();
-		
-		b1.varPublic=41;
-		b1.methodPublic();
-		
-		
-		//Instance Operator
-		System.out.println(b1.instance);
+	b1.varDefault=11;
+	b1.methodDefault();
+	// private member accessing
+	  /*b1.varPrivate=21;
+	b1.mehtodPrivate(); */
+	
+	b1.varProtected=31;
+	b1.methodProtected();
+	
+    b1.varPublic=41;
+    b1.methodPublic();
+    
+    
+   // instance operator
+    System.out.println(b1 instanceof Base);   //true 
+    
+	
+	
+	
+	
 
-		
-		
+
 	}
 
 }
